feb4fde75010a78cff8f22766bd19b1ef1ab112b
EasonChen <yschen1124@gmail.com>
2024-08-29 19:25:34 +0800

    add heartbeat led event

--- src/MicroOcpp/Model/Heartbeat/HeartbeatService.cpp
+++ src/MicroOcpp/Model/Heartbeat/HeartbeatService.cpp
@@ -8,26 +8,30 @@
 #include <MicroOcpp/Core/Configuration.h>
 #include <MicroOcpp/Operations/Heartbeat.h>
 #include <MicroOcpp/Platform.h>
-
+#include "myevent.h"
 using namespace MicroOcpp;
 
-HeartbeatService::HeartbeatService(Context& context) : MemoryManaged("v16.Heartbeat.HeartbeatService"), context(context) {
+HeartbeatService::HeartbeatService(Context &context) : MemoryManaged("v16.Heartbeat.HeartbeatService"), context(context)
+{
     heartbeatIntervalInt = declareConfiguration<int>("HeartbeatInterval", 86400);
     lastHeartbeat = mocpp_tick_ms();
 
-    //Register message handler for TriggerMessage operation
-    context.getOperationRegistry().registerOperation("Heartbeat", [&context] () {
-        return new Ocpp16::Heartbeat(context.getModel());});
+    // Register message handler for TriggerMessage operation
+    context.getOperationRegistry().registerOperation("Heartbeat", [&context]()
+                                                     { return new Ocpp16::Heartbeat(context.getModel()); });
 }
 
-void HeartbeatService::loop() {
+void HeartbeatService::loop()
+{
     unsigned long hbInterval = heartbeatIntervalInt->getInt();
-    hbInterval *= 1000UL; //conversion s -> ms
+    hbInterval *= 1000UL; // conversion s -> ms
     unsigned long now = mocpp_tick_ms();
 
-    if (now - lastHeartbeat >= hbInterval) {
+    if (now - lastHeartbeat >= hbInterval)
+    {
         lastHeartbeat = now;
-
+        esp32m::HeartBeatEvent ev;
+        esp32m::EventManager::instance().publish(ev);
         auto heartbeat = makeRequest(new Ocpp16::Heartbeat(context.getModel()));
         context.initiateRequest(std::move(heartbeat));
     }
